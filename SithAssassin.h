3d3ea39123f806e5f6f4cef3f89cbfeddd0a1b38
yank7 <yan-eric.cossette@productionszani.com>
2016-05-03 14:40:48 -0400

    Comments finished for sithassasin

--- SithAssassin.h
+++ SithAssassin.h
@@ -10,11 +10,32 @@
 
 class SithAssassin : public SorcierSith {
 public:
+    /* Constructeur */
     SithAssassin();
+    /* Constructeur avec un nom */
     SithAssassin(std::string nom);
+    /* Destructeur */
     ~SithAssassin();
+
+    /*
+     * Entrée: Personnage que nous voulons attaquer
+     * Sortie: Retourne l'objet attaque qui contien tous l'information nécessaire
+     * Rôle: Permet au personnage passé en argument de reçevoir des dégats, s'occupe aussi du coût de la force
+     */
     Attaque attaqueAveuglement(Personnage &cible);
+
+    /*
+     * Entrée: Personnage que nous voulons attaquer
+     * Sortie: Retourne l'objet attaque qui contien tous l'information nécessaire
+     * Rôle: Permet au personnage passé en argument de reçevoir des dégats, s'occupe aussi du coût de la force
+     */
     Attaque attaqueSabreLaserDouble(Personnage &cible);
+
+    /*
+     * Entrée: Aucune
+     * Sortie: Sort un vecteur de tous les attaques
+     * Rôle: Sera utile pour le choix des attaques
+     */
     virtual std::vector <std::string> getAttaques();
 };
 
