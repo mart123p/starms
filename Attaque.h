8b27574ae3e1a00e36ea048449847fca9ace7931
yank7 <yan-eric.cossette@productionszani.com>
2016-05-03 14:26:47 -0400

    Commentaire de attaque.h

--- Attaque.h
+++ Attaque.h
@@ -10,16 +10,38 @@
 
 class Attaque {
 public:
+    /* Constructeur */
     Attaque();
-    Attaque(std::string nom, int degats, int coupsDeForce, int pourcentageReussite);
+    /* Constructeur avec le nom de l'attaque, le nombre dégats, le couts en force, pourcentage de réussite pour l'attaque*/
+    Attaque(std::string nom, int degats, int coutsDeForce, int pourcentageReussite);
+    /*
+     * Entrée: Aucune
+     * Sortie: Dit si l'attaque à réussi ou non (avec le pourcentage de réussite)
+     * Rôle: Dit si l'attaque à réussi ou non
+    */
     bool successfullAttaque();
+    /*
+     * Entrée: Aucune
+     * Sortie: Retourne le nombre de dégats
+     * Rôle: retourne le nombre de dégats suite au test de réussite de l'attaque
+    */
     int getDegats();
-    int getCoupsDeForce();
+    /*
+     * Entrée: Aucune
+     * Sortie: Retourne le cout de l'attaque en force
+     * Rôle: Permet de calculer le cout de l'attaque pour que plus-tard savoir si le personnage pourra faire son attaque ou non
+    */
+    int getCoutsDeForce();
+    /*
+     * Entrée: Aucune
+     * Sortie: Retourne le nom de l'attaque en question
+     * Rôle: Permet de connaitre le nom du personnage en question
+    */
     std::string getNoM();
 
 private:
     int m_degats;
-    int m_coupsDeForce;
+    int m_coutsDeForce;
     int m_pourcentageReussite;
     std::string m_nom;
 };
