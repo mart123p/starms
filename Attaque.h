6a5ba12a34c54ebdbfa75db80f2e74a1c8dba65a
Martin Pouliot <martinp507@gmail.com>
2016-05-03 13:58:52 -0400

    fix

---
+++ Attaque.h
@@ -0,0 +1,25 @@
+//
+// Created by Martin Pouliot on 2016-05-03.
+//
+
+#ifndef STARMS_ATTAQUE_H
+#define STARMS_ATTAQUE_H
+
+
+#include <string>
+
+class Attaque {
+public:
+    Attaque();
+    Attaque(std::string nom, int degats, int coupsDeForce, int pourcentageReussite);
+    bool successfullAttaque();
+
+private:
+    int m_degats;
+    int m_coupsDeForce;
+    int m_pourcentageReussite;
+    std::string m_nom;
+};
+
+
+#endif //STARMS_ATTAQUE_H
---
+++ ChevalierJedi.h
@@ -0,0 +1,19 @@
+//
+// Created by Martin Pouliot on 2016-05-03.
+//
+
+#ifndef STARMS_CHEVALIERJEDI_H
+#define STARMS_CHEVALIERJEDI_H
+
+
+#include "Personnage.h"
+
+class ChevalierJedi : public Personnage {
+    ChevalierJedi(std::string);
+    ChevalierJedi();
+    ~ChevalierJedi();
+
+};
+
+
+#endif //STARMS_CHEVALIERJEDI_H
