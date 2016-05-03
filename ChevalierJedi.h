e2ead537472457ecefde20df37adbc5757b3d6b0
Martin Pouliot <martinp507@gmail.com>
2016-05-03 14:36:53 -0400

    Added comments for ChevalierJedi

--- ChevalierJedi.h
+++ ChevalierJedi.h
@@ -11,12 +11,39 @@
 
 class ChevalierJedi : public Personnage {
 public:
+    /* Constructeur avec nom */
     ChevalierJedi(std::string nom);
+    /* Constructeur sans nom */
     ChevalierJedi();
+    /* Destructeur */
     ~ChevalierJedi();
-    Attaque attaqueProjection(Personnage &cible);
-    Attaque attaqueGuerison(Personnage &cible);
+
+    /*
+     *  Entrée: Le personnage à qui les dégats doivent être reçus
+     *  Sortie: Objet Attaque qui contient tous les détails du sort projection
+     *  Rôle: Permet au personnage passé en argument de recevoir des dégats, s'occupe aussi du coût de la force de l'attaque.
+     */
+    Attaque sortProjection(Personnage &cible);
+
+    /*
+     *  Entrée: null
+     *  Sortie: Objet Attaque qui contient tous les détails du sort
+     *  Rôle: Permet au personnage de se guérir, s'occupe aussi du coût de la force.
+     */
+    Attaque sortGuerison();
+
+    /*
+     *  Entrée: Le personnage à qui les dégats doivent être reçus
+     *  Sortie: Objet Attaque qui contient tous les détails de l'attaque sabre laser
+     *  Rôle: Permet au personnage passé en argument de recevoir des dégats, s'occupe aussi du coût de la force.
+     */
     Attaque attaqueSabreLaser(Personnage &cible);
+
+    /*
+     * Entrée: Aucune
+     * Sortie: Sort un vecteur de tous les attaques
+     * Rôle: Sera utile pour le choix des attaques
+     */
     virtual std::vector <std::string> getAttaques();
 
 };
