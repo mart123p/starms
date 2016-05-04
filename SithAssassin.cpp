a1e8ca06caf5853d1d969612d127f000dd0c9e5c
yank7 <yan-eric.cossette@productionszani.com>
2016-05-04 15:30:13 -0400

    bonjour 2

--- SithAssassin.cpp
+++ SithAssassin.cpp
@@ -1,1 +1,20 @@
 #include "SithAssassin.h"
+
+SithAssassin::SithAssassin() {
+
+}
+SithAssassin::SithAssassin(std::string nom) {
+
+}
+SithAssassin::~SithAssassin() {
+
+}
+Attaque SithAssassin::attaqueAveuglement(Personnage &cible) {
+
+}
+Attaque SithAssassin::attaqueSabreLaserDouble(Personnage &cible) {
+
+}
+std::vector <std::string> SithAssassin::getAttaques() {
+
+}
