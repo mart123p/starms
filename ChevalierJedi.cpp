355044ec1358c533e79cd7a1c59418cca2b9d74d
yank7 <yan-eric.cossette@productionszani.com>
2016-05-04 15:09:04 -0400

    creates the .cpp basic file of chevalierJedi

--- ChevalierJedi.cpp
+++ ChevalierJedi.cpp
@@ -1,1 +1,23 @@
 #include "ChevalierJedi.h"
+
+ChevalierJedi::ChevalierJedi() {
+
+}
+ChevalierJedi::ChevalierJedi(std::string nom) {
+
+}
+ChevalierJedi::~ChevalierJedi() {
+
+}
+Attaque ChevalierJedi::sortProjection(Personnage &cible) {
+
+}
+Attaque ChevalierJedi::sortGuerison() {
+
+}
+Attaque ChevalierJedi::attaqueSabreLaser(Personnage &cible) {
+
+}
+std::vector <std::string> ChevalierJedi::getAttaques() {
+
+}
