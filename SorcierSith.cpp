10526627746370ad55d96a625616c1adc0bcba6f
yank7 <yan-eric.cossette@productionszani.com>
2016-05-04 15:22:01 -0400

    SorcierSith class cree

--- SorcierSith.cpp
+++ SorcierSith.cpp
@@ -1,5 +1,19 @@
-//
-// Created by Martin Pouliot on 2016-05-03.
-//
-
 #include "SorcierSith.h"
+SorcierSith::SorcierSith() {
+
+}
+SorcierSith::SorcierSith(std::string nom) {
+
+}
+SorcierSith::~SorcierSith() {
+
+}
+Attaque SorcierSith::attaqueDechargeElectrique(Personnage &cible) {
+
+}
+Attaque SorcierSith::attaqueCouteauLaser(Personnage &cible) {
+
+}
+std::vector <std::string> SorcierSith::getAttaques() {
+
+}
