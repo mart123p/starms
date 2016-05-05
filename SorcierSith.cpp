f116307da8a78baad892fa64e2a3fd3f329a5ee8
yank7 <yan-eric.cossette@productionszani.com>
2016-05-05 14:44:53 -0400

    socirfsdfsd

--- SorcierSith.cpp
+++ SorcierSith.cpp
@@ -16,8 +16,16 @@ SorcierSith::~SorcierSith() {
 
 }
 Attaque SorcierSith::attaqueDechargeElectrique(Personnage &cible) {
-
+    Attaque attaque("D\x82charge \x82lectrique", lancerDe(2, 20), lancerDe(1, 20), 75);
+    if(attaque.isSuccessfulAttaque()){
+        cible.recevoirDegats(attaque.getDegats());
+    }
+    return attaque;
 }
 Attaque SorcierSith::attaqueCouteauLaser(Personnage &cible) {
-
+    Attaque attaque("Couteau laser", lancerDe(2, 4), 0, 70);
+    if(attaque.isSuccessfulAttaque()){
+        cible.recevoirDegats(attaque.getDegats());
+    }
+    return attaque;
 }
