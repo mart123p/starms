40a110a402d0e2ceed258e314c0414efaf48b26c
yank7 <yan-eric.cossette@productionszani.com>
2016-05-05 14:17:37 -0400

    remove const

--- SorcierSith.cpp
+++ SorcierSith.cpp
@@ -8,12 +8,12 @@ SorcierSith::SorcierSith(std::string nom) {
 SorcierSith::~SorcierSith() {
 
 }
-Attaque SorcierSith::attaqueDechargeElectrique(Personnage &cible) const {
+Attaque SorcierSith::attaqueDechargeElectrique(Personnage &cible) {
 
 }
-Attaque SorcierSith::attaqueCouteauLaser(Personnage &cible) const {
+Attaque SorcierSith::attaqueCouteauLaser(Personnage &cible) {
 
 }
-std::vector <std::string> SorcierSith::getAttaques() const{
+std::vector <std::string> SorcierSith::getAttaques() {
 
 }
