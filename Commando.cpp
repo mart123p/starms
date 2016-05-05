f97d49f280f2cbcf712909ae36101a71c57967d1
yank7 <yan-eric.cossette@productionszani.com>
2016-05-05 14:08:10 -0400

    Robert est fini

--- Commando.cpp
+++ Commando.cpp
@@ -1,10 +1,14 @@
 #include "Commando.h"
 
 Commando::Commando() {
-
+    m_vieMax = lancerDe(7, 20);
+    m_vie = m_vieMax;
+    m_nom = "Robert";
 }
 Commando::Commando(std::string nom) {
-
+    m_nom = nom;
+    m_vieMax = lancerDe(7, 20);
+    m_vie = m_vieMax;
 }
 Commando::~Commando() {
 
